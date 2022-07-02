e6bee285b89f4d4f7d7af53a587516532044d3e2
Ebuks1 <chukwuebukaagu1@gmail.com>
2022-07-01 17:49:34 -0700

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 	char letter;
-	{
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-	}
 	{
-		if (letter != 'e' && letter != 'q')
+		if (letter != 'e' || letter == 'q')
+			continue;
+		else
 			putchar(letter);
 	}
-	{
 	putchar('\n');
-	}
 
 	return (0);
 }
