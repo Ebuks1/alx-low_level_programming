d5a3ed9deb7c1d113ebee5941f6777a8b798388e
Ebuks1 <chukwuebukaagu1@gmail.com>
2022-07-01 17:26:07 -0700

    Alph new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints a to z and A to Z.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
-
+	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
 		putchar(letter);
-
-	putchar ('\n')
-
+	}
+	{
+	putchar('\n');
+	}
 	return (0);
 }
