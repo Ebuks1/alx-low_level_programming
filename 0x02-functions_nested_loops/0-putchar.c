f0451f2540062a1f146c071b02c596ce4fc7d163
Ebuks1 <chukwuebukaagu1@gmail.com>
2022-07-05 13:29:00 -0700

    New putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - prints _putchar
- * Return: Always 0 (success)
+ * print_alphabet - print a-z
+ *
+ * Return: Always void
  */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 
-	return (0);
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
